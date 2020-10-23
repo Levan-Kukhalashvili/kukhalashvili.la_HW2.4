95a6097c7011106ef81b943fe414de917186527e
KukhalashviliLA <48877065+KukhalashviliLA@users.noreply.github.com>
2020-10-23 09:18:40 +0300

    Initial Commit

---
+++ kukhalashvili.la_HW2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kukhalashvili.la_HW2.4
+//
+//  Created by Admin on 23.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
